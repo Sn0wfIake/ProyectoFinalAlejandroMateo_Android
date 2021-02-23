cbff05db71b9d63e747b8ca6c772838b8cc298a0
alex <alexmatemese@gmail.com>
2021-02-23 23:41:26 +0100

    Activity agenda y calendario creados,

---
+++ app/src/main/java/com/example/proyectofinalalejandromateo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.proyectofinalalejandromateo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button botoncalendario = (Button) findViewById(R.id.calendario);
+        botoncalendario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), calendario.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+        Button botonagenda = (Button) findViewById(R.id.agenda);
+        botonagenda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), agenda.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+}
